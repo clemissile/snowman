bcf207cac506fba70ca81afccb2c6177495fd892
Ganivet Clement <21402554@C304L-417C06.campus.unicaen.fr>
2019-12-10 15:32:37 +0100

    Un peu de style

--- client/src/components/Case/Case.vue
+++ client/src/components/Case/Case.vue
@@ -1,12 +1,24 @@
 <template>
-    <div v-if="value === 'P'"><v-icon>mdi-human-handsup</v-icon></div>
-    <div v-else-if="value === 'L'"><v-icon>mdi-circle-small</v-icon></div>
-    <div v-else-if="value === 'M'"><v-icon style="font-size:22pt;">mdi-circle-medium</v-icon></div>
-    <div v-else-if="value === 'B'"><v-icon>mdi-circle</v-icon></div>
-    <div v-else-if="value === 'LM'"><v-icon>mdi-circle-small</v-icon><v-icon style="font-size:22pt;">mdi-circle-medium</v-icon></div>
-    <div v-else-if="value === 'MB'"><v-icon style="font-size:22pt;">mdi-circle-medium</v-icon><v-icon>mdi-circle</v-icon></div>
-    <div v-else-if="value === 'LMB'"><v-icon>mdi-snowman</v-icon></div>
-    <div v-else>{{ value }}</div>
+    <div v-if="value === 'P'" class="cell" id="player"><v-icon>mdi-human-handsup</v-icon></div>
+
+    <div v-else-if="value === 'L'" class="cell ball"><v-icon style="font-size:22pt;">mdi-circle-medium</v-icon></div>
+
+    <div v-else-if="value === 'M'" class="cell ball"><v-icon style="font-size:42pt;">mdi-circle-medium</v-icon></div>
+
+    <div v-else-if="value === 'B'" class="cell ball"><v-icon style="font-size:25pt;">mdi-circle</v-icon></div>
+
+    <div v-else-if="value === 'LM'" class="cell ball">
+        <v-row><v-icon style="font-size:22pt;">mdi-circle-medium</v-icon></v-row><br>
+        <v-row><v-icon style="font-size:42pt;">mdi-circle-medium</v-icon></v-row>
+    </div>
+
+    <div v-else-if="value === 'MB'" class="cell ball">
+        <v-icon style="font-size:42pt;">mdi-circle-medium</v-icon>
+        <v-icon style="font-size:25pt;">mdi-circle</v-icon>
+    </div>
+
+    <div v-else-if="value === 'LMB'" class="cell ball"><v-icon>mdi-snowman</v-icon></div>
+    <div v-else class="cell">{{ value }}</div>
 </template>
 
 <script src="./lib/case.js"></script>
